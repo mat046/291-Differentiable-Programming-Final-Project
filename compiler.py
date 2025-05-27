97cfe8f46ef4c7236f1b0bfaae337e379736c0b9
Matthew Thomas <matthew.a.thomas2002@gmail.com>
2025-05-26 23:18:54 -0700

    I think it outputs a string that is correct. Need to implement compiling to shared lib now

--- compiler.py
+++ compiler.py
@@ -22,50 +22,60 @@ import platform
 import distutils.ccompiler
 import subprocess
 
-def loma_to_ctypes_type(t : floma_diff_ir.type | floma_diff_ir.arg, \
-                        _dfloat : ctypes.Structure) -> ctypes.Structure:
-    """ Given a loma type, maps to the corresponding ctypes type by
-        looking up ctypes_structs
-    """
+# def loma_to_ctypes_type(t : floma_diff_ir.type | floma_diff_ir.arg, \
+#                         _dfloat : ctypes.Structure) -> ctypes.Structure:
+#     """ Given a loma type, maps to the corresponding ctypes type by
+#         looking up ctypes_structs
+#     """
 
-    match t:
-        case floma_diff_ir.Arg():
-            return loma_to_ctypes_type(t.t, _dfloat)
-        # case floma_diff_ir.Int():
-        #     return ctypes.c_int
-        case floma_diff_ir.Float():
-            return ctypes.c_float
-        case floma_diff_ir.Struct():
-            return _dfloat
-        case floma_diff_ir.Cont():
-            return ctypes.CFUNCTYPE(None, ctypes.POINTER(_dfloat))
-        case None:
-            return None
-        case _:
-            assert False
+#     match t:
+#         case floma_diff_ir.Arg():
+#             return loma_to_ctypes_type(t.t, _dfloat)
+#         # case floma_diff_ir.Int():
+#         #     return ctypes.c_int
+#         case floma_diff_ir.Float():
+#             return ctypes.c_float
+#         case floma_diff_ir.Struct():
+#             return _dfloat
+#         case floma_diff_ir.Cont():
+#             return ctypes.CFUNCTYPE(None, ctypes.POINTER(_dfloat))
+#         case None:
+#             return None
+#         case _:
+#             assert False
 
 
-def get_function_symbols(so_file : str) -> list[str]:
-    symbol_output = subprocess.run(
-            ['nm', '-D', '--defined-only', so_file],
-            stdout=subprocess.PIPE,
-            text=True
-        ).stdout
-    symbols_table = [line for line in symbol_output.splitlines()]
-    split_cols = [line.split(" ") for line in symbols_table]
-    symbols_list = [line[2] for line in split_cols]
-    return symbols_list
+# def get_function_symbols(so_file : str) -> list[str]:
+#     symbol_output = subprocess.run(
+#             ['nm', '-D', '--defined-only', so_file],
+#             stdout=subprocess.PIPE,
+#             text=True
+#         ).stdout
+#     symbols_table = [line for line in symbol_output.splitlines()]
+#     split_cols = [line.split(" ") for line in symbols_table]
+#     symbols_list = [line[2] for line in split_cols]
+#     return symbols_list
 
-def find_function_symbol(so_file_path : str, func_names : list[str]) -> dict[str, str]:
-    symbols_list = get_function_symbols(so_file_path)
+# def find_function_symbol(so_file_path : str, func_names : list[str]) -> dict[str, str]:
+#     symbols_list = get_function_symbols(so_file_path)
     
-    func_name_to_symbol = {}
-    for n in func_names:
-        matches = [smbl for smbl in symbols_list if n in smbl]
-        match = min(matches, key=len)
-        func_name_to_symbol[n] = match
+#     func_name_to_symbol = {}
+#     for n in func_names:
+#         matches = [smbl for smbl in symbols_list if n in smbl]
+#         match = min(matches, key=len)
+#         func_name_to_symbol[n] = match
 
-    return func_name_to_symbol
+#     return func_name_to_symbol
+
+
+def define_interface(funcs : list[floma_diff_ir.FunctionDef]) -> str:
+    code = ""
+    for f in funcs:
+        code += f"m.def(\"{f.id}\", &{f.id},"
+        arg_list = [f"py::arg(\"{a.id}\")" for a in f.args]
+        args = ",".join(arg_list)
+        code += args + ");\n\n"
+    return code
 
 
 def compile(loma_code : str,
@@ -74,25 +84,27 @@ def compile(loma_code : str,
             output_cpp_filename : str = None,
             print_error = True):
     """ Given loma frontend code represented as a string,
-        compiles it to either C, ISPC, or OpenCL code.
-        Furthermore, generates a library from the compiled code,
-        and dynamically links the generated library.
+        compiles C++.
+        It then generates a shared library from the compiled code
+        which can be imported as a python module.
 
         Parameters:
         loma_code - a string representing loma code to be compiled
-        target - 'c', 'ispc', or 'opencl'
-        output_filename - where to store the generated library for C and ISPC backends. 
+        target - 'c++' only for now
+        output_filename - where to store the generated library 
             Don't need the suffix (like '.so').
-            when target == 'opencl', this argument is ignored.
-        opencl_context, opencl_device, opencl_command_queue - see cl_utils.create_context()
-                    only used by the opencl backend
-        print_error - whether it prints compile errors or not
+        output_cpp_filename - where to store the c++ 'intermediate' file
+            This is entirely optional - use for debugging purposes.
     """
+
+    # 
+    interfaces : set[str]
 
     # The compiler passes
     # first parse the frontend code
     try:
         funcs = parser.parse(loma_code)
+        interfaces = {f.id for f in funcs.values() if isinstance(f, floma_diff_ir.ReverseDiff)}
 
         # next figure out the types related to differentiation
         dfloat, funcs = autodiff.make_builtins(funcs)
@@ -131,6 +143,7 @@ def compile(loma_code : str,
 #include <functional>
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
+#include <memory>
         \n""" + code
 
         # add pybind11 module; used to turn cpp file into an importable python module
@@ -138,14 +151,24 @@ def compile(loma_code : str,
 namespace py = pybind11;
 
 PYBIND11_MODULE(test_floma_module, m) {
-    py::class_<_dfloat>(m, "_dfloat")
+    py::class_<_dfloat, std::shared_ptr<_dfloat>>(m, "_dfloat")
         .def(py::init<>())
         .def_readwrite("val", &_dfloat::val)
         .def_readwrite("dval", &_dfloat::dval);
 
-    m.def("make__dfloat", &make__dfloat);
-    m.def("make__const__dfloat", &make__const__dfloat, py::return_value_policy::reference);
-    m.def("d_func", &d_func);
+    m.def("make__dfloat", &make__dfloat,
+          py::arg("val"), py::arg("dval"),
+          "Create a _dfloat with specified value and derivative.");
+
+    m.def("make__const__dfloat", &make__const__dfloat,
+          py::arg("val"),
+          "Create a constant _dfloat (zero derivative).");
+
+"""
+        to_be_interfaces = [f for f in funcs.values() if f.id in interfaces]
+        code += define_interface(to_be_interfaces)
+
+        code += """
 }
 """
 
